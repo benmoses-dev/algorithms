113f30f98f1433e48cf942caccbe8d7c8eb4af35
Ben M <ben@mosesweb.co.uk>
2025-11-08 20:36:12 +0000

    feat: Start recursive Cooley-Tukey forward FFT. Need to implement inverse and add iterative versions.

--- cpp/numbers/include/fourier.hpp
+++ cpp/numbers/include/fourier.hpp
@@ -26,3 +26,55 @@ inline std::vector<std::complex<double>> dft(std::vector<double> &re) {
     }
     return transform;
 }
+
+inline std::vector<std::complex<double>> fftRec(std::vector<double> &re) {
+    const u64 N = re.size();
+    if (N == 1) {
+        return {std::complex<double>(re[0], 0.0)};
+    }
+    std::vector<double> E;
+    E.reserve(N / 2);
+    std::vector<double> O;
+    O.reserve(N / 2);
+    for (u64 i = 0; i < N; i++) {
+        if (i % 2 == 0) {
+            E.emplace_back(re[i]);
+            continue;
+        }
+        O.emplace_back(re[i]);
+    }
+    auto even = fftRec(E);
+    auto odd = fftRec(O);
+    std::vector<std::complex<double>> transform(N);
+    for (u64 k = 0; k < (N / 2); k++) {
+        const double tau = 2 * std::numbers::pi;
+        const double freq = static_cast<double>(k) / static_cast<double>(N);
+        const double angle = tau * freq;
+        const double twiddleRe = std::cos(angle);
+        const double twiddleIm = std::sin(-angle);
+        const double oddRe = odd[k].real();
+        const double oddIm = odd[k].imag();
+        const double tempRe = twiddleRe * oddRe - twiddleIm * oddIm;
+        const double tempIm = twiddleRe * oddIm + twiddleIm * oddRe;
+        transform[k] =
+            std::complex<double>(even[k].real() + tempRe, even[k].imag() + tempIm);
+        transform[k + (N / 2)] =
+            std::complex<double>(even[k].real() - tempRe, even[k].imag() - tempIm);
+    }
+    return transform;
+}
+
+inline std::vector<std::complex<double>> fft(std::vector<double> &re) {
+    const u64 N = re.size();
+    if ((N & N - 1) != 0) {
+        u64 p = 2;
+        while (p < N) {
+            p <<= 1;
+        }
+        const u64 diff = p - N;
+        for (u64 i = 0; i < diff; i++) {
+            re.emplace_back(0.0);
+        }
+    }
+    return fftRec(re);
+}
