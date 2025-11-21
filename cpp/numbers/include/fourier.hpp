9e0762fa977e5eb15b759957089814920a79288a
Ben M <ben@mosesweb.co.uk>
2025-11-21 12:06:45 +0000

    feat: Inverse DFT and FFT

--- cpp/numbers/include/fourier.hpp
+++ cpp/numbers/include/fourier.hpp
@@ -7,65 +7,72 @@
 #include <vector>
 
 using u64 = std::uint64_t;
+const double TAU = 2 * std::numbers::pi;
 
-inline std::vector<std::complex<double>> dft(std::vector<double> &re) {
-    const u64 N = re.size();
-    std::vector<std::complex<double>> transform;
-    transform.reserve(N);
-    const double tau = 2 * std::numbers::pi;
+inline std::vector<std::complex<double>> dft(const std::vector<double> &in,
+                                             const bool inverse) {
+    const u64 N = in.size();
+    std::vector<std::complex<double>> spectrum;
+    spectrum.reserve(N);
     for (u64 k = 0; k < N; k++) {
         const double freq = static_cast<double>(k) / static_cast<double>(N);
         double sumRe = 0.0;
         double sumIm = 0.0;
         for (u64 n = 0; n < N; n++) {
-            const double angle = tau * static_cast<double>(n) * freq;
-            sumIm -= std::sin(angle) * re[n];
-            sumRe += std::cos(angle) * re[n];
+            const double angle = TAU * static_cast<double>(n) * freq * (inverse ? 1 : -1);
+            sumIm += std::sin(angle) * in[n];
+            sumRe += std::cos(angle) * in[n];
         }
-        transform.emplace_back(sumRe, sumIm);
+        spectrum.emplace_back(sumRe, sumIm);
     }
-    return transform;
+    if (inverse) {
+        for (auto &f : spectrum) {
+            f /= static_cast<double>(spectrum.size());
+        }
+    }
+    return spectrum;
 }
 
-inline std::vector<std::complex<double>> fftRec(std::vector<double> &re) {
-    const u64 N = re.size();
+inline std::vector<std::complex<double>>
+fftRec(const std::vector<std::complex<double>> &in, const bool inverse) {
+    const u64 N = in.size();
     if (N == 1) {
-        return {std::complex<double>(re[0], 0.0)};
+        return in;
     }
-    std::vector<double> E;
+    std::vector<std::complex<double>> E;
     E.reserve(N / 2);
-    std::vector<double> O;
+    std::vector<std::complex<double>> O;
     O.reserve(N / 2);
     for (u64 i = 0; i < N; i++) {
-        if (i % 2 == 0) {
-            E.emplace_back(re[i]);
+        if ((i & 1) == 0) {
+            E.emplace_back(in[i]);
             continue;
         }
-        O.emplace_back(re[i]);
+        O.emplace_back(in[i]);
     }
-    auto even = fftRec(E);
-    auto odd = fftRec(O);
-    std::vector<std::complex<double>> transform(N);
+    auto even = fftRec(E, inverse);
+    auto odd = fftRec(O, inverse);
+    std::vector<std::complex<double>> spectrum(N);
     for (u64 k = 0; k < (N / 2); k++) {
-        const double tau = 2 * std::numbers::pi;
         const double freq = static_cast<double>(k) / static_cast<double>(N);
-        const double angle = tau * freq;
+        const double angle = TAU * freq * (inverse ? 1 : -1);
         const double twiddleRe = std::cos(angle);
-        const double twiddleIm = std::sin(-angle);
+        const double twiddleIm = std::sin(angle);
         const double oddRe = odd[k].real();
         const double oddIm = odd[k].imag();
         const double tempRe = twiddleRe * oddRe - twiddleIm * oddIm;
         const double tempIm = twiddleRe * oddIm + twiddleIm * oddRe;
-        transform[k] =
+        spectrum[k] =
             std::complex<double>(even[k].real() + tempRe, even[k].imag() + tempIm);
-        transform[k + (N / 2)] =
+        spectrum[k + (N / 2)] =
             std::complex<double>(even[k].real() - tempRe, even[k].imag() - tempIm);
     }
-    return transform;
+    return spectrum;
 }
 
-inline std::vector<std::complex<double>> fft(std::vector<double> &re) {
-    const u64 N = re.size();
+inline std::vector<std::complex<double>> fft(std::vector<std::complex<double>> &in,
+                                             const bool inverse) {
+    const u64 N = in.size();
     if ((N & N - 1) != 0) {
         u64 p = 2;
         while (p < N) {
@@ -73,8 +80,14 @@ inline std::vector<std::complex<double>> fft(std::vector<double> &re) {
         }
         const u64 diff = p - N;
         for (u64 i = 0; i < diff; i++) {
-            re.emplace_back(0.0);
+            in.emplace_back(0.0);
         }
     }
-    return fftRec(re);
+    std::vector<std::complex<double>> spectrum = fftRec(in, inverse);
+    if (inverse) {
+        for (auto &f : spectrum) {
+            f /= static_cast<double>(spectrum.size());
+        }
+    }
+    return spectrum;
 }
